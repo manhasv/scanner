51e49a59a52fb7adc4f54178530936ea503520f9
manh <manhasv1501@gmail.com>
2026-07-07 22:41:46 +0700

    added gamma correction, detect corners, and warp the document

--- contour.py
+++ contour.py
@@ -1,35 +1,34 @@
 import numpy as np
 import cv2
 import pillow_heif
+import sys
 
-INPUT = "./input/test_img2.HEIC"
-RATIO = 0
-IMAGE = None
+INPUT = "./input/test2.HEIC"
+
 def main():
     # adding file check later for other extensions
     heif_file = pillow_heif.open_heif(INPUT, convert_hdr_to_8bit=False, bgr_mode=True)
     image = np.asarray(heif_file)
+    IMAGE = image
 
-    processed_img = preprocess(image)
-    
+    processed_img = preprocess(IMAGE)
     contours = detect_contours(processed_img)
 
-    sorted_contours = sorted(
-        contours,
-        key=cv2.contourArea,
-        reverse=True
-    )
-    output = image.copy()
-    hull = cv2.convexHull(sorted_contours[0])
-    draw_contours(output, [hull], "./output/contour.png")
-    print(hull.shape)
+    approx, corners = detect_doc_contour(contours)
+    
+    draw_contours(IMAGE.copy(), [approx], "./output/contour.png")
+    draw_corners(IMAGE.copy(), corners)
+    
+    warp(IMAGE.copy(), corners)
+
+
 
 def resize_img(image):
     height, width = image.shape[:2]
     target_height = 500
     proportion = target_height/ float(height)
     target_width = int(width * proportion)
-    resized = cv2.resize(image, (target_height, target_width), interpolation=cv2.INTER_AREA)
+    resized = cv2.resize(image, (target_width, target_height), interpolation=cv2.INTER_AREA)
     return resized
 
 def preprocess(image):
@@ -39,7 +38,7 @@ def preprocess(image):
 
     # apply gamma correction using the lookup table
     gray = cv2.LUT(gray, table)
-
+    cv2.imwrite('output/gray.png', gray)
     ret,thresh = cv2.threshold(gray,80,255,cv2.THRESH_BINARY)
     output = image.copy()
     cv2.imwrite('output/thresh.png', thresh)
@@ -66,6 +65,92 @@ def detect_contours(image):
 def draw_contours(image, contours, output):
     cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
     cv2.imwrite(output, image)
+
+def detect_doc_contour(contours):
+    sorted_contours = sorted(
+        contours,
+        key=cv2.contourArea,
+        reverse=True
+    )
+    
+    hull = cv2.convexHull(sorted_contours[0])
+    print(f"contour {sorted_contours[0].shape}")
+    print(f"hull {hull.shape}")
+
+    # approx to curve it down to 4 corners
+    length = 0
+    perimeter = cv2.arcLength(hull, True)
+    var = 0.02
+    
+    # while length != 4:
+    #     if length < 4:
+    #         var += 0.005
+    #     else:
+    #         var -= 0.005
+    # Missing solution when the captured image is not a rectangle or shadow is clipped in
+    epsilon = var * perimeter
+    approx = cv2.approxPolyDP(hull, epsilon, True)
+    
+    if len(approx) != 4:
+        print(f"Error length of detected contour is {len(approx)}")
+        sys.exit(-1)
+    
+    corners = detect_corners(approx)
+
+    return approx, corners
+    
+def detect_corners(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+
+    pts = pts.reshape(4, 2)
+    s = pts.sum(axis=1)
+
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    # Top-Right will have the smallest difference
+    rect[1] = pts[np.argmin(diff)]
+    # Bottom-Left will have the largest difference
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+    
+def draw_corners(output, corners):
+    for i, corner in enumerate(corners):
+        x = int(corner[0])
+        y = int(corner[1])
+        cv2.circle(output, (x, y), radius=5, color=(0, 0, 255), thickness=-1)
+        text_position = (x + 10, y - 10)
+    
+        cv2.putText(
+            output, 
+            text=str(i + 1),
+            org=text_position,  
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+            fontScale=0.6,     
+            color=(0, 0, 255),  
+            thickness=2  
+        )
+    
+    cv2.imwrite("output/corners.png", output)
+
+def warp(image, corners):
+    TL, TR, BR, BL = corners
+
+    width = max(np.linalg.norm(BR - BL), np.linalg.norm(TR - TL))
+    height = width * 1.414
+
+    matrix = np.float32([
+        [0, 0],
+        [width - 1, 0],
+        [width - 1, height - 1],
+        [0, height - 1],
+    ])
+    M = cv2.getPerspectiveTransform(corners, matrix, solveMethod=None)
+    
+    warped_image = cv2.warpPerspective(image, M, (int(width), int(height)))
+    cv2.imwrite("./output/warped.png", warped_image)
 
 if __name__ == '__main__':
     main()
